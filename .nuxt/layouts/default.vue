3f6bd82efcc3c9a8b88323499666cd3aabe39328
athrys5 <capriotti.andrea00@gmail.com>
2022-10-04 23:18:30 +0200

    v1.2

--- .nuxt/layouts/default.vue
+++ .nuxt/layouts/default.vue
@@ -3,24 +3,29 @@
     <nav class="navbar navbar-dark bg-dark">
       <div> 
         <b-button 
-          v-b-toggle.sidebar-no-header 
+          v-b-toggle.sidebar-no-header
           class="btn">
             <span class="navbar-toggler-icon"></span>
         </b-button>
         <b-sidebar 
           id="sidebar-no-header"
+          aria-labelledby="sidebar-no-header-title"
+          no-header
+          shadow
           width="450px">
           <template #default="{ hide }">
-            <div class="p-3">
+            <div class="p-3 idk">
               <h4 id="sidebar-no-header-title">Game</h4>
+            </div>
+            <div class="p-3">
               <nav class="mb-3">
                 <b-nav vertical>
-                  <b-nav-item active @click="hide">Active</b-nav-item>
-                  <b-nav-item href="#link-1" @click="hide">Link</b-nav-item>
-                  <b-nav-item href="#link-2" @click="hide">Another Link</b-nav-item>
+                  <b-nav-item class="item-menu" @click="hide">I tuoi Amici</b-nav-item>
+                  <b-nav-item class="item-menu" @click="hide">Giochi</b-nav-item>
+                  <b-nav-item class="item-menu" @click="hide">Video</b-nav-item>
                 </b-nav>
               </nav>
-              <b-button variant="primary" block @click="hide">Close Sidebar</b-button>
+              <b-button block @click="hide">Close Sidebar</b-button>
             </div>
           </template>
         </b-sidebar>
