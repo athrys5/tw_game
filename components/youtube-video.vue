21875412763e1590183142f92227047ebbec57e4
athrys5 <capriotti.andrea00@gmail.com>
2022-10-06 16:28:10 +0200

    v3 video

--- components/youtube-video.vue
+++ components/youtube-video.vue
@@ -1,11 +1,41 @@
 <template>
     <div class="video-container">
-        <b-button @click="getVideos">cerca</b-button>
-        <div>
-            <p v-for="item in vlist" :key="item.id">
-                {{item.snippet.title}}
-            </p>
-        </div>
+        <section class="create-todo">
+			<h3>CREATE A TODO</h3>
+
+			<form id="new-todo-form">
+				<div class="options">
+					<label>
+						<input 
+							type="radio" 
+							id="category1" 
+							value="animal documentaries"
+						/>
+						<span class="bubble business"></span>
+						<div>Business</div>
+					</label>
+					<label>
+						<input 
+							type="radio" 
+							id="category2" 
+							value="funny animal videos"
+						/>
+						<span class="bubble personal"></span>
+						<div>Personal</div>
+					</label>
+
+				</div>
+				<b-button v-on:click="getVideos">codio</b-button>
+			</form>
+		</section>
+        <iframe v-for="video in vlist" 
+            :key="video.id.videoId" 
+            width="400" 
+            height="315" 
+            :src="'https://www.youtube.com/embed/'+video.id.videoId"  
+            allowfullscreen
+            >
+        </iframe>
     </div>
 </template>
 
@@ -33,29 +63,49 @@ fetch("https://youtube.googleapis.com/youtube/v3/search?key="+ API_KEY +"&videoD
         data() {
             return{
                 vlist: [],
+                setCategory: "",
+                isChecked: false,
             }
         },
         methods:{
             getVideos(){
-                this.vlist = []
-                axios.request({
-                    method: 'GET',
-                    url: 'https://www.googleapis.com/youtube/v3/search',
-                    params: {
-                        part: 'snippet',
-                        key: "AIzaSyD2vaibQ0DVyEdo8x0_aCv-tUYsNzARdFU",
-                        q: "funnyvideos",
-                        maxResults: 5,
-                        type: 'video'
-                    },
-                })
-                .then((response) =>{
-                    this.vlist = response.data.items
-                    console.log(this.vlist)
-                }).catch(function (error) {
-                    console.log(error)
-                });
+                this.pickCategory()
+                if(!this.isChecked){
+                    this.vlist = []
+                    axios.request({
+                        method: 'GET',
+                        url: 'https://www.googleapis.com/youtube/v3/search',
+                        params: {
+                            part: 'snippet',
+                            key: "AIzaSyCUq1SbbgrOTyIoAxq-O9l5eGx6SRHL-n0",
+                            q: this.setCategory,
+                            maxResults: 4,
+                            type: 'video',
+                            videoDuration: "short",
+                            videoEmbeddable: true,
+                        },
+                    })
+                    .then((response) =>{
+                        this.vlist = response.data.items
+                    }).catch(function (error) {
+                        console.log(error)
+                    });
+                }
             },
+            pickCategory(){
+                this.isChecked = false;
+                const doc = document.getElementById('category1');
+                const fun = document.getElementById('category2');
+                if(doc.checked){
+                    this.setCategory = doc.value
+                }else if(fun.checked){
+                    this.setCategory = fun.value
+                }else{
+                    this.isChecked = true
+                }
+            }
         }
     }
 </script>
+
+<style src="./youtube-video.css"></style>
