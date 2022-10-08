b268b84395e5658c5a3783182b4f09dd02afc116
athrys5 <capriotti.andrea00@gmail.com>
2022-10-08 19:03:26 +0200

    v3.2 Video

--- components/youtube-video.vue
+++ components/youtube-video.vue
@@ -1,43 +1,58 @@
 <template>
     <div>
-        <div class="container-fluid form-check">
-                <div class="row justify-content-around extCon">
-                    <div class="col-12 col-sm-5 shadow p-3 mb-5 bg-white rounded">
-                        <label>
-                            <input
-                                type="radio" 
-                                name="category" 
-                                id="category1"
-                                value="animal documentaries"
-                                >
-                            <div>
-                                Documentaries
-                            </div>
-                        </label>
-                    </div>
-                    <div class="col-12 col-sm-5 shadow mb-5 bg-white rounded">
-                        <label>
-                            <input
-                                type="radio" 
-                                name="category" 
-                                id="category2"
-                                value="funny animal videos"
+        <div class="container-fluid col-10 firstContainer">
+            <div class="row justify-content-center extCon">
+                <div class="col-12 col-sm-6 form-check">
+                    <label class="shadow-lg">
+                        <input
+                            type="radio" 
+                            name="category" 
+                            id="category1"
+                            value="animal documentaries"
                             >
-                            <div>Funny</div>
-                        </label> 
-                    </div>
-                    <button class="button-1" v-on:click="getVideos">Search</button>
-                </div> 
+                        <span class="bubble business"></span>
+                        <div class="catTitles">
+                            <h4 class="CustomCat">Documentaries</h4>
+                        </div>
+                    </label>
+                </div>
+            </div>
+            <div class="row justify-content-center extCon2">
+                <div class="col-12 col-sm-6 form-check">
+                    <label class="shadow-lg">
+                        <input
+                            type="radio" 
+                            name="category" 
+                            id="category2"
+                            value="funny animal videos"
+                        >
+                        <span class="bubble personal"></span>
+                        <div class="catTitles">
+                            <h4 class="CustomCat text-wrap">Funny</h4>
+                        </div>
+                    </label> 
+                </div>
+            </div>
+            <div class="row justify-content-center">
+                <div class="col-12 col-sm-6 formButton">
+                    <button class="button-1 shadow-lg" v-on:click="getVideos">Search</button>
+                </div>
+            </div>
         </div>
-        <div class="container-fluid">
-            <iframe v-for="video in vlist" 
-                :key="video.id.videoId" 
-                width="400" 
-                height="315" 
-                :src="'https://www.youtube.com/embed/'+video.id.videoId"  
-                allowfullscreen
-                >
-            </iframe>
+        <div class="container-fluid col-12 col-sm-8 col-lg-6">
+            <div class="row card cardCustom shadow-sm" v-for="video in vlist" :key="video.id.videoId" >
+                <iframe
+                    class = "videoSize"
+                    frameBorder="0"
+                    :src="'https://www.youtube.com/embed/'+video.id.videoId"  
+                    allowfullscreen
+                    >
+                </iframe>
+                <div class="card-body">
+                    <h5 class="card-title text-left">{{video.snippet.title}}</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                </div>
+            </div>
         </div>
     </div>
 </template>
